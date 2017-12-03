c2ff7f71a91783fa28c6ac320a8504ffde210174
jessica <jessica.mcinchak@gmail.com>
2017-12-02 20:49:30 -0500

    bump react-date version, more date picker rules

---
+++ src/components/MetricsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MetricsList extends Component {
+  render() {
+    return (
+      <div>
+        <p>Next, fetch some data...</p>
+      </div>
+    )
+  }
+}
+
+export default MetricsList;
---
+++ src/components/Picker.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { DateRangePicker } from 'react-dates';
+import moment from 'moment';
+
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+import MetricsList from './MetricsList';
+
+class Picker extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      startDate: moment().subtract(1, 'months'),
+      endDate: moment(),
+      focusedInput: null,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <DateRangePicker
+          startDateId={'start'}
+          startDate={this.state.startDate}
+          endDateId={'end'}
+          endDate={this.state.endDate}
+          onDatesChange={({ startDate, endDate }) => { this.setState({ startDate, endDate })}}
+          focusedInput={this.state.focusedInput}
+          onFocusChange={(focusedInput) => { this.setState({ focusedInput })}}
+          isOutsideRange={() => false}
+          minimumNights={0}
+          numberOfMonths={2}
+          showClearDates={true}
+          block={false}
+        />
+        { this.state.startDate && this.state.endDate ? 
+          <MetricsList start={this.state.startDate.format('MM-DD-YYYY')} end={this.state.endDate.format('MM-DD-YYYY')} /> : <p>Please select a date range.</p> }
+      </div>
+    );
+  }
+}
+
+export default Picker;
